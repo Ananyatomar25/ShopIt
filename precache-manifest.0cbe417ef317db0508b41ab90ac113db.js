4abfc93bd416c6629d447b9ab879b5a72bbd3d88
Ananya Tomar <ananyatomar205@gmail.com>
2022-06-26 15:04:33 +0530

    Updates

---
+++ precache-manifest.0cbe417ef317db0508b41ab90ac113db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a323c6a6920c0047fd192d13b61506",
+    "url": "/ShopIt/index.html"
+  },
+  {
+    "revision": "9db8fdbb9e4d3ec591e8",
+    "url": "/ShopIt/static/css/main.0c8489f7.chunk.css"
+  },
+  {
+    "revision": "395ad0a20edab9dc6123",
+    "url": "/ShopIt/static/js/2.4ba8082c.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/ShopIt/static/js/2.4ba8082c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db8fdbb9e4d3ec591e8",
+    "url": "/ShopIt/static/js/main.5f6b2c89.chunk.js"
+  },
+  {
+    "revision": "c9d74d0f37b610d4e98e",
+    "url": "/ShopIt/static/js/runtime-main.66ce1e2c.js"
+  }
+]);
